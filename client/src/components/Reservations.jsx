3f231503d80a65f0034aa36640ddd59efdb74ec1
Harin Rana <harinrana24@gmail.com>
2024-04-14 15:35:11 -0400

    mmm

--- client/src/components/Reservations.jsx
+++ client/src/components/Reservations.jsx
@@ -38,8 +38,9 @@ const ReservationsList = () => {
 
         setReservationData(prev => ({
             ...prev,
-            filteredReservations: filtered,
+            filteredReservations: filtered instanceof Array ? filtered : [],
         }));
+        
     };
 
     if (errorr) {
